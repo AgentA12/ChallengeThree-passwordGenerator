5efc4c3000a02c333b7bc08a0907383c97829fae
Andrew Howran <andrewhowrandev17@gmail.com>
2022-02-28 20:16:52 -0500

    changed file structure and started lengthPrompt function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,7 @@
 // Assignment code here
-
+generatePassword = function(){
+  var specialCharacters = ['!', '#', '$', '@', '%', '^', '*'];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +17,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+var lengthPrompt = function() {
+  var passwordLength = window.prompt("What would you like the length of your password to be? (Minimum 8, Max 128 characters)");
+  passwordLength = parseInt(passwordLength);
+  console.log(Number.isInteger(passwordLength));
+  
+    
+
+}
+
+lengthPrompt();
